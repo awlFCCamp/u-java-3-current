b78b2e35621798d31a8980aa7386b75bd6948cd7
Angela Lee <angelawlee@rocketmail.com>
2023-04-18 18:16:07 -0500

    current progress

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -0,0 +1,51 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import com.udacity.jwdnd.course1.cloudstorage.entity.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/signup")
+@AllArgsConstructor
+public class SignupController {
+    private final UserService userService;
+
+    @GetMapping
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping
+    public String signupUser(@ModelAttribute User user, Model model, RedirectAttributes redirectAttributes) {
+        String error = "";
+
+        if (!userService.isUsernameAvailable(user.getUsername())) {
+            error = "The username already exists.";
+        }
+
+        try{
+            userService.createUser(user);
+            redirectAttributes.addFlashAttribute("signupSuccess", true);
+        }catch(Exception e){
+            error="Signup exception occurred";
+            System.out.println("Signup exception occurred");
+        }
+
+        if (error.isEmpty()) {
+            model.addAttribute("signupSuccess", true);
+        } else {
+            model.addAttribute("signupError", error);
+        }
+
+        return "signup";
+
+        }
+
+    }
